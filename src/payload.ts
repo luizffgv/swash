343e60a76ce99d115ca39d57ccff88f952a41f18
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-05-16 18:27:05 -0300

    refactor: Remove useless undefined and constructor from payload.ts

--- src/payload.ts
+++ src/payload.ts
@@ -7,16 +7,12 @@ export abstract class Payload<T = unknown> {
    * Constructs the payload with the given value.
    * @param value - Value of the payload.
    */
-  constructor(value: T) {
-    this.value = value;
+  constructor(...value: T extends undefined ? [] | [T] : [T]) {
+    this.value = value[0] as T;
   }
 }
 
 /** A payload with no value. */
-export class EmptyPayload extends Payload<undefined> {
-  constructor() {
-    super(undefined);
-  }
-}
+export class EmptyPayload extends Payload<undefined> {}
 
 export default Payload;
